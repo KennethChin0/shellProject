5e1840c3df31a26ef4156fc71706d23501005dc4
Minuk Kim <mkim01@stuy.edu>
2019-12-03 11:06:56 -0500

    working on forking and exec

--- shell.c
+++ shell.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 
 void main(int argc, char *argv[]){
-
 	int i;
+
 	if (argc < 2){
 		printf("no arg\n");
 		exit(1);
 	}
 
 	for(int i=1; i<argc; i++){
-		printf("%s " , argv[i]);
-		printf("\n");
-	}
+		int pid = fork();
+		if (pid < 0){
+			perror("fork failed");
+			exit(1);
+		}
+		else if (pid == 0){
+			printf("%s " , argv[i]);
+			printf("\n");
+			execv(argv[0], argv);
+		}
 }
 
